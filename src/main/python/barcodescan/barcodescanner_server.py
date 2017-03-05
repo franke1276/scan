945b3592b45c0527bede769fe03ecc9b41625dd6
Christian Franke <chriss@frankeonline.net>
2017-03-05 21:37:10 +0100

    refactoring

--- src/main/python/barcodescan/barcodescanner_server.py
+++ src/main/python/barcodescan/barcodescanner_server.py
@@ -8,6 +8,8 @@ from flask_sse import sse
 import json
 import uuid
 import redis
+from barcodescan.WorkerDao import WorkerDao
+from barcodescan.ScannedDataService import ScannedDataService
 
 def generate_or_get_unique_id(file_path):
   if not os.path.isfile(file_path):
@@ -19,65 +21,29 @@ def generate_or_get_unique_id(file_path):
 
 app = Flask(__name__)
 app.config["REDIS_URL"] = "redis://localhost"
-home_dir=os.environ['HOME']
-app.logger.info("HOME: {}".format(home_dir))
-app.config["UUID"] = generate_or_get_unique_id(home_dir + "/.barcodescanner")
 logging.basicConfig(level=logging.DEBUG)
 r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+dao = WorkerDao(r)
 
+home_dir=os.environ['HOME']
+service = ScannedDataService(generate_or_get_unique_id(home_dir + "/.barcodescanner"), dao)
 
 app.register_blueprint(sse, url_prefix='/stream')
 
 
 @app.route("/", methods=['GET'])
 def index():
-  worker_id = app.config["UUID"]
-  worker = r.hgetall(worker_id)
-  if not worker:
-    worker = {"id": worker_id}
-    r.hmset(worker_id, worker)
-  if not "jobs" in worker:
-    jobs=[]
-  else:
-    jobs = worker["jobs"].split(",")
-
-  if not "stage" in worker:
-    stage="-"
-  else:
-    stage=worker["stage"]
-
-  if not "employee" in worker:
-    employee="-"
-  else:
-    employee=worker["employee"]
-
-
-  return render_template('index.html', event_type="scans", worker_id=worker["id"], employee=employee,
-                         stage=stage, jobs=jobs)
+  worker = service.get_worker()
+  return render_template('index.html', event_type="scans", worker_id=worker["key"], employee=worker.get("employee", "-"),
+                         stage=worker.get("stage", "-"), jobs=worker.get("jobs", []))
 
 
 @app.route("/", methods=['PUT'])
 def put_data():
   last_scan = request.get_json()["data"]
   app.logger.debug("last scan: {}".format(last_scan))
-  worker_id = app.config["UUID"]
-  worker = r.hgetall(worker_id)
-  if not worker:
-    worker = {"id": worker_id}
-    r.hmset(worker_id, worker)
 
-  splitted_str = last_scan.split('-')
-  if splitted_str[0] == "mit":
-    worker["employee"] = splitted_str[1]
-  if splitted_str[0] == "platz":
-    worker["stage"] = splitted_str[1]
-  if splitted_str[0] == "a":
-    other_jobs = []
-    if "jobs" in worker:
-      other_jobs=worker["jobs"].split(",")
-    other_jobs.append(splitted_str[1])
-    worker["jobs"]=",".join(set(other_jobs))
-  r.hmset(worker_id, worker)
+  worker = service.put_scanned_data(last_scan)
 
   message = json.dumps({"message": worker})
   sse.publish(message, type='scans')
