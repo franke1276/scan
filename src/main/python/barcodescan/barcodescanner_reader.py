c9c80f96f14f6ce77cbb48f777094a22940db277
Christian Franke <chriss@frankeonline.net>
2017-03-10 17:12:25 +0100

    use config file for reader

--- src/main/python/barcodescan/barcodescanner_reader.py
+++ src/main/python/barcodescan/barcodescanner_reader.py
@@ -8,8 +8,7 @@ from barcodescan.Config import Config
 def main():
   reader = BarcodeReader("/dev/hidraw0")
   signal.signal(signal.SIGTERM, lambda x: reader.close())
-  home_dir = os.environ['HOME']
-  config = Config(home_dir + "/.barcodescanner.cfg")
+  config = Config("/home/pi/.barcodescanner.cfg")
   push_client = Push_client(config.reader_push_url())
   for line in reader.read_generator():
     push_client.push_data({'data': line})
